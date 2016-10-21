8a7a40027c9064dc5488bd2b8299453a6b441a83
dadaa <dadaaism@gmail.com>
2016-10-21 17:59:49 +0900

    Add scene-mayreturn

---
+++ scene-mayreturn/scene-mayreturn.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var container = document.querySelector("#scene-mayreturn");
+  var hoshiNumber = 5 + Math.floor(Math.random() * 10);
+  for (var i = 0; i < hoshiNumber; i++) {
+    createHoshiAnimation(container);
+  }
+});
+
+function createHoshiAnimation(container) {
+  var hoshi = document.createElement("div");
+  hoshi.classList.add("hoshi");
+  hoshi.style.left = Math.floor(Math.random() * 100) + "%";
+  hoshi.style.top = Math.floor(Math.random() * 100) + "%";
+  container.appendChild(hoshi);
+  var duration = 1000 + Math.floor(Math.random() * 1000);
+  var easing = "linear";
+  switch (Math.floor(Math.random() * 4)) {
+    case 0 : {
+      easing = "ease-in";
+      break;
+    }
+    case 1 : {
+      easing = "ease-out";
+      break;
+    }
+    case 2 : {
+      easing = "ease-in-out";
+      break;
+    }
+  }
+  var animation = hoshi.animate({ transform: ["scale(0.1)", "scale(1)"],
+                                 opacity: [1, 0] },
+                               { easing: "ease-in",
+                                 duration: duration,
+                                 fill: "forwards",
+                                 iterations: Infinity });
+  if (animation.finished) {
+    animation.effect.timing.iterations = 1;
+    animation.finished.then(function() {
+      hana.remove();
+      createHoshiAnimation(container);
+    });
+  }
+}
